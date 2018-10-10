5bef189bc932704e6bcd3e6b75512bd3cb4bf81f
CESDE <CESDE@PC-24>
2018-10-10 08:37:15 -0500

    se ha terminado de agregar los beans principales

---
+++ AplicacionPrueba/src/Beans/Persona.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author CESDE
+ */
+public class Persona {
+
+    private int idpersona;
+    private String nombre;
+    private String apepaterno;
+    private String apematerno;
+
+    /**
+     * @return the idpersona
+     */
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    /**
+     * @param idpersona the idpersona to set
+     */
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apepaterno
+     */
+    public String getApepaterno() {
+        return apepaterno;
+    }
+
+    /**
+     * @param apepaterno the apepaterno to set
+     */
+    public void setApepaterno(String apepaterno) {
+        this.apepaterno = apepaterno;
+    }
+
+    /**
+     * @return the apematerno
+     */
+    public String getApematerno() {
+        return apematerno;
+    }
+
+    /**
+     * @param apematerno the apematerno to set
+     */
+    public void setApematerno(String apematerno) {
+        this.apematerno = apematerno;
+    }
+
+}
